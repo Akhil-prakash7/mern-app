b09c9f998fd0928706588ed84128c9073b41ab78
Akhil-prakash7 <akhilprakashm4@gmail.com>
2023-12-24 17:53:06 +0530

    created custom error middleware

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRoute.js';
 import authRouter from './routes/authRoute.js'
+import { errorHandler } from './midlewares/errorHandler.js';
 dotenv.config()
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log('database connected')
@@ -18,3 +19,7 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter)
+
+
+
+app.use(errorHandler)
