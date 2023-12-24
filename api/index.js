38bebeedd0f3d24c21b01189868dbe57aa79e030
Akhil-prakash7 <akhilprakashm4@gmail.com>
2023-12-24 17:32:53 +0530

    created signin functioanality

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRoute.js';
+import authRouter from './routes/authRoute.js'
 dotenv.config()
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log('database connected')
@@ -9,9 +10,11 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log(err)
 });
 const app = express();
+app.use(express.json())
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000')
 });
 
-app.use('/api/user',userRouter)
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter)
