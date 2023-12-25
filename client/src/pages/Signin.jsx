95c936798dd38acb937c6371262993bce6443b7a
Akhil-prakash7 <akhilprakashm4@gmail.com>
2023-12-25 21:26:52 +0530

    added redux-persist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
+import { useDispatch ,useSelector} from 'react-redux';
+import { signinStart,signinSuccess,signinFailure } from '../redux/user/userSlice';
 
 const SignIn = () => {
   const [formData, setFormData] = useState({
@@ -7,9 +9,9 @@ const SignIn = () => {
     email: '',
     password: '',
   });
-  const [error,setError] = useState(null);
-  const [loading,setLoading] = useState(false)
+  const {loading,error} = useSelector((state)=>state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     const { id, value } = e.target;
@@ -18,75 +20,81 @@ const SignIn = () => {
       [id]: value,
     }));
   };
-  const handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-   try {
-    setLoading(true)
-    const res = await fetch('/api/auth/signin',
-    {
-        method:'POST',
-        headers:{
-            'Content-type':'application/json',
-        },
-        body:JSON.stringify(formData),
-    });
-    const data = await res.json();
-    if(data.success ==false){
-        setError(data.message);
-        setLoading(false);
-        return;
-    }
-    setLoading(false)
-    setError(null)
-    navigate('/')
   
-   } catch (error) {
-    setLoading(false)
-    setError(error.message)
-   }
-}
+    try {
+      dispatch(signinStart());
+  
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      if (!res.ok) {
+        // Handle non-successful HTTP status codes
+        const data = await res.json();
+        dispatch(signinFailure(data.message || 'Signin failed'));
+        return;
+      }
+  
+      const data = await res.json();
+      
+      if (data.success === false) {
+        dispatch(signinFailure(data.message));
+        return;
+      }
+  
+      dispatch(signinSuccess(data));
+      navigate('/');
+    } catch (error) {
+      // Handle general errors
+      dispatch(signinFailure(error.message || 'Signin failed'));
+    }
+  };
+  
 
   console.log(formData)
   
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        
-        <input
-          type='email'
-          placeholder='email'
-          className='border p-3 rounded-lg'
-          id='email'
-          onChange={handleChange}
-          value={formData.email}
-        />
-        <input
-          type='password'
-          placeholder='password'
-          className='border p-3 rounded-lg'
-          id='password'
-          onChange={handleChange}
-          value={formData.password}
-        />
-        <button 
-          disabled={loading}
-          className='bg-green-300 p-3 rounded-lg hover:bg-green-800 hover:text-white font-semibold disabled:opacity-70'
-        >{loading?'Loading...':'Sign In'}
-          
-        </button>
-      </form>
-      
-      <div className='flex mt-5'>
-        <p>Dont have an Account?</p>
-        <Link to='/sign-up'>
-          <span className='text-blue-700 ml-2'>SignUp</span>
-        </Link>
-        
-      </div>
-      {error && <p className='text-red-500'>{error}</p>}
+    <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+      <input
+        type='email'
+        placeholder='email'
+        className='border p-3 rounded-lg'
+        id='email'
+        onChange={handleChange}
+        value={formData.email}
+      />
+      <input
+        type='password'
+        placeholder='password'
+        className='border p-3 rounded-lg'
+        id='password'
+        onChange={handleChange}
+        value={formData.password}
+      />
+      <button
+        disabled={loading}
+        className='bg-green-300 p-3 rounded-lg hover:bg-green-800 hover:text-white font-semibold disabled:opacity-70'
+      >
+        {loading ? 'Loading...' : 'Sign In'}
+      </button>
+    </form>
+    <div className='flex mt-5'>
+      <p>Dont have an Account?</p>
+      <Link to='/sign-up'>
+        <span className='text-blue-700 ml-2'>SignUp</span>
+      </Link>
     </div>
-  );
+    {error && <p className='text-red-500'>{error}</p>}
+  </div>
+);
 };
 
 export default SignIn;
